32abe4482759b3d30c8ab29a360500c6c37307f4
Rithankoushik <koushikrithan@gmail.com>
2025-09-12 17:44:05 +0530

    updated code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_chroma import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain_ollama import OllamaLLM
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema import Document
